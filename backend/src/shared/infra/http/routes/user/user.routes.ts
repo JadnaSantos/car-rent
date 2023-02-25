f31a0bdc0761d32469f3dd88222eb45beefb342c
Jadna Santos <jadnajesus7@gmail.com>
2023-02-25 20:06:59 -0300

    feat: add nova rota

--- backend/src/shared/infra/http/routes/user/user.routes.ts
+++ backend/src/shared/infra/http/routes/user/user.routes.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express';
-import { makeSignInController } from '../../../factories/sign-in';
-import { makeSignUpController } from '../../../factories/sign-up';
 import { adaptRoute } from '../../config/adapter';
+import { makeSignUpController } from '../../../factories/sign-up';
+import { makeSignInController } from '../../../factories/sign-in';
+import { makeProfilleController } from '../../../factories/profille';
+import { authenticate } from '../../middlewares/authenticate';
+import { makeUserAvatarController } from '../../../factories/user-avatar';
 
 const usersRouterSignUp = Router();
 const usersRouterSignIn = Router();
+const userProfilleRouter = Router();
+const userAvatarRouter = Router();
+
 
 usersRouterSignUp.post('/', adaptRoute(makeSignUpController()));
 usersRouterSignIn.post('/', adaptRoute(makeSignInController()));
+userProfilleRouter.get('/', authenticate, adaptRoute(makeProfilleController()));
+userAvatarRouter.patch('/', authenticate, adaptRoute(makeUserAvatarController()));
 
 
-export { usersRouterSignUp, usersRouterSignIn };
+export {
+  usersRouterSignUp,
+  usersRouterSignIn,
+  userProfilleRouter,
+  userAvatarRouter
+};
