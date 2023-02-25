ede80df64003fa0cbc1047faa663714c1c8e70a7
Jadna Santos <jadnajesus7@gmail.com>
2023-02-25 20:00:39 -0300

    feat: adicionado a rota de user avatar

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,8 +1,20 @@
 import multer from 'multer';
 import { Router } from 'express';
-import { createCarRouter, listCarRouter, detailsRouter, deleteCarRouter, updateCarRouter, finishCarRouter } from './car/car.routes';
 import uploadConfig from '.././../../../config/multer';
-import { usersRouterSignIn, usersRouterSignUp } from './user/user.routes';
+import {
+  createCarRouter,
+  listCarRouter,
+  detailsRouter,
+  deleteCarRouter,
+  updateCarRouter,
+  finishCarRouter
+} from './car/car.routes';
+import {
+  userAvatarRouter,
+  userProfilleRouter,
+  usersRouterSignIn,
+  usersRouterSignUp
+} from './user/user.routes';
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -10,6 +22,8 @@ const upload = multer(uploadConfig);
 
 routes.use('/users', usersRouterSignUp);
 routes.use('/signin', usersRouterSignIn);
+routes.use('/profile', userProfilleRouter);
+routes.use('/user/avatar', upload.single('avatar'), userAvatarRouter);
 routes.use('/cars', upload.single('file'), createCarRouter);
 routes.use('/list-cars', listCarRouter);
 routes.use('/cars', detailsRouter);
