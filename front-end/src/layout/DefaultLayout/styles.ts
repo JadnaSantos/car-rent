b65519dfecc4a78b1a909a18a6fdaee4830c0239
Jadna Santos <jadnajesus7@gmail.com>
2023-03-17 10:12:23 -0300

    refactor: remove arquivo front-end

--- front-end/src/layout/DefaultLayout/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const LayoutContainer = styled.div`
-  max-width: min(100rem, 100vw);
-  margin-inline: auto;
-`;
--- front-end/src/pages/Home/index.tsx
+++
@@ -1,8 +0,0 @@
-function Home() {
-  return (
-    <h1>This is home</h1>
-  );
-}
-
-
-export { Home };
