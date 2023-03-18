74bba48fbe768feee4d546403c2c020925a1fb80
Jadna Santos <jadnajesus7@gmail.com>
2023-03-18 12:20:27 -0300

    add swaggerUi ao app

--- backend/src/shared/infra/http/config/app.ts
+++ backend/src/shared/infra/http/config/app.ts
@@ -3,11 +3,17 @@ import { cors } from '../middlewares/cors';
 import { routes } from '../routes';
 import path from 'path';
 
+import swaggerFile from '../../http/swagger.json';
+import swaggerUi from 'swagger-ui-express';
+
 const app = express();
 app.use(express.json());
 app.use(cors);
 app.use(routes);
 app.use('/files', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
 
 
 export { app };
